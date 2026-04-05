32ee2e707196d7d56f8ad554f31ef5b0290ee6ed
RakshitaMuttur <193498450+RakshitaMuttur@users.noreply.github.com>
2026-04-05 20:23:07 +0530

    Time: 22 ms (9.01%) | Memory: 9.1 MB (4.50%) - LeetSync

--- 2122-count-special-quadruplets/count-special-quadruplets.c
+++ 2122-count-special-quadruplets/count-special-quadruplets.c
@@ -3,13 +3,18 @@ int countQuadruplets(int* nums, int numsSize)
     int count=0;
     for(int i=0; i<numsSize-3; i++)
     {
+        int a=nums[i];
         for(int j=i+1; j<numsSize-2; j++)
         {
+            int b=nums[j];
             for(int k=j+1; k<numsSize-1;k++)
             {
+                int c=nums[k];
                 for(int l=k+1; l<numsSize;l++)
                 {
-                    if(nums[i]+nums[j]+nums[k]==nums[l]) count++;
+                    int d=nums[l];
+                    int s=a+b+c;
+                    if(s==d) count++;
                 }
             }
         }
